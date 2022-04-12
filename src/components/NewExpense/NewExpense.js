3252a2a941e92aa824939dfdfc078cd3224460b6
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-04-12 19:38:06 +0600

    added expense adding form

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { ExpForm } from './ExpForm'
+
+import './NewExpense.css'
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpForm />
+    </div>
+  )
+}
+
+export default NewExpense
