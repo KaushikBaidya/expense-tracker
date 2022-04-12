2cb64a59f50eac354f9c6c1482b5b49c75998e91
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-04-12 15:36:20 +0600

    Update ExpenseItem.js

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,9 @@ import { ExpenseDate } from './ExpenseDate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+  const handleClick = () => {
+    console.log('clicked!')
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -13,6 +16,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={handleClick}>Change Title</button>
     </Card>
   )
 }
