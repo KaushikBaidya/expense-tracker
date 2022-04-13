2f2072630fedf337878d3e70451b166ffa56ac23
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-04-13 09:15:13 +0600

    added conditional rendering through external files

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import ExpenseFilter from './ExpenseFilter'
 
 import './Expense.css'
+import ExpenseList from './ExpenseList'
 
 const Expense = (props) => {
   const [filteredYear, setFilteredYear] = useState('2022')
@@ -17,26 +17,13 @@ const Expense = (props) => {
     return expense.date.getFullYear().toString() === filteredYear
   })
 
-  let expensesContent = <p>No Expenses Found</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={filteredYear}
         onChangeFilter={handleFilterChange}
       />
-      {expensesContent}
+      <ExpenseList expenses={filteredExpenses} />
     </Card>
   )
 }
